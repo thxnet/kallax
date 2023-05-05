329c3a03cbf0b95a3732c6487f6e58166a2722d7
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-05-05 10:00:17 +0800

    feat(tracker/client): reimplement `Tracker` client

---
+++ tracker/client/src/error/mod.rs
@@ -0,0 +1,22 @@
+mod leafchain_peer;
+mod leafchain_spec;
+mod rootchain_peer;
+mod rootchain_spec;
+
+use snafu::{Backtrace, Snafu};
+
+pub use self::{
+    leafchain_peer::{GetLeafchainPeerAddressError, InsertLeafchainPeerAddressError},
+    leafchain_spec::GetLeafchainSpecError,
+    rootchain_peer::{GetRootchainPeerAddressError, InsertRootchainPeerAddressError},
+    rootchain_spec::GetRootchainSpecError,
+};
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility(pub))]
+pub enum Error {
+    #[snafu(display("Error occurs while connecting to tracker, error: {source}"))]
+    ConnectToTrackerGrpc { source: tonic::transport::Error, backtrace: Backtrace },
+}
---
+++ tracker/client/src/error/rootchain_spec.rs
@@ -0,0 +1,25 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum GetRootchainSpecError {
+    Primitives { source: kallax_primitives::Error },
+
+    Status { source: tonic::Status },
+
+    MissingRootchainSpec,
+}
+
+impl From<kallax_primitives::Error> for GetRootchainSpecError {
+    #[inline]
+    fn from(source: kallax_primitives::Error) -> Self { Self::Primitives { source } }
+}
+
+impl fmt::Display for GetRootchainSpecError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Primitives { source } => source.fmt(f),
+            Self::Status { source } => source.fmt(f),
+            Self::MissingRootchainSpec => f.write_str("Missing rootchain spec"),
+        }
+    }
+}
