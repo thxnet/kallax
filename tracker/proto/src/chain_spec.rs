bf1561d766198f12c5ebe4b0088bbc2e2476a438
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-05-05 08:43:46 +0800

    feat(tracker/proto): separate `Rootchain` and `Leafchain`

--- tracker/proto/src/chain_spec.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::proto::{
-    chain_spec_service_client::ChainSpecServiceClient,
-    chain_spec_service_server::{ChainSpecService, ChainSpecServiceServer},
-    ChainLayer, ChainMetadata, ChainSpec, GetChainSpecRequest, GetChainSpecResponse,
-};
