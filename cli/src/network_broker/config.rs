8e0429c9a85595b03978d6cc358cb1a1d5983bc0
graysurf <terry@thxlab.io>
2023-08-11 19:41:24 +0800

    fix(cli/network-broker): use `default_value` instead of `default_value_t`

--- cli/src/network_broker/config.rs
+++ cli/src/network_broker/config.rs
@@ -4,9 +4,9 @@ use crate::network_broker::{CONFIG_PATH, TRACKER_API_ENDPOINT};
 
 #[derive(Args, Debug)]
 pub struct Config {
-    #[clap(long = "tracker-api-endpoint", help = "Tracker api endpoint", default_value_t = TRACKER_API_ENDPOINT.parse::<http::Uri>().unwrap())]
+    #[clap(long = "tracker-api-endpoint", help = "Tracker api endpoint", default_value = TRACKER_API_ENDPOINT)]
     pub tracker_api_endpoint: http::Uri,
 
-    #[clap(long = "file", help = "Config file path", default_value_t = String::from(CONFIG_PATH))]
+    #[clap(long = "file", help = "Config file path", default_value = CONFIG_PATH)]
     pub file: String,
 }
