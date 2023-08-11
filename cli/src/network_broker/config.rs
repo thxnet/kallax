57a3cc191888b95620798c30bf0ef627ed4351e6
graysurf <terry@thxlab.io>
2023-08-11 18:51:16 +0800

    fix(cli/network-broker): fix name of `tracker_api_endpoint`

--- cli/src/network_broker/config.rs
+++ cli/src/network_broker/config.rs
@@ -4,8 +4,8 @@ use crate::network_broker::{CONFIG_PATH, TRACKER_API_ENDPOINT};
 
 #[derive(Args, Debug)]
 pub struct Config {
-    #[clap(long = "tracker-grpc-endpoint", help = "Tracker gRPC endpoint", default_value_t = TRACKER_API_ENDPOINT.parse::<http::Uri>().unwrap())]
-    pub tracker_grpc_endpoint: http::Uri,
+    #[clap(long = "tracker-api-endpoint", help = "Tracker api endpoint", default_value_t = TRACKER_API_ENDPOINT.parse::<http::Uri>().unwrap())]
+    pub tracker_api_endpoint: http::Uri,
 
     #[clap(long = "file", help = "Config file path", default_value_t = String::from(CONFIG_PATH))]
     pub file: String,
