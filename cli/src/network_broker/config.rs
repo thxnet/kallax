350f899c0f50d41ab0c2ff90563d6230458712ed
graysurf <terry@thxlab.io>
2023-08-20 12:24:48 +0000

    feat(cli/network-broker): add `short name` on arg `file`

--- cli/src/network_broker/config.rs
+++ cli/src/network_broker/config.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::Args;
 
 use crate::network_broker::{CONFIG_PATH, TRACKER_API_ENDPOINT};
@@ -7,6 +9,6 @@ pub struct Config {
     #[clap(long = "tracker-api-endpoint", help = "Tracker api endpoint", default_value = TRACKER_API_ENDPOINT)]
     pub tracker_api_endpoint: http::Uri,
 
-    #[clap(long = "file", help = "Config file path", default_value = CONFIG_PATH)]
-    pub file: String,
+    #[clap(short = 'f', long = "file", help = "Config file path", default_value = CONFIG_PATH)]
+    pub file: PathBuf,
 }
